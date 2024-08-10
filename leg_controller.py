7700264dfa6cb27fecbb7407dec94fd62f0459da
jack33001 <51167808+jack33001@users.noreply.github.com>
2024-08-09 18:49:14 -0700

    A lot works. A lot doesn't.

--- leg_controller.py
+++ leg_controller.py
@@ -6,9 +6,11 @@ from scipy.special import comb
 class leg_controller:
     # A mid-level controller for control of each leg. Commands are given to this selfect by instances of the "gait_scheduler" selfect, and this selfect
     # gives comands to the "pd" selfect.
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.curr_pos = np.array([np.pi / 4, np.pi / 4 * -2])
-        self.curr_vel = 0
+        self.curr_vel = np.array([0, 0])
+
         # all/most controllers
         self.v_des = 1  # desired velocity
         self.contact = 0  # boolean, is the foot touching the ground?
@@ -20,78 +22,98 @@ class leg_controller:
         self.yaw_vel = 0  # the robot's yaw velocity
         self.forw_vel = 0  # the robot's forward velocity
         self.foot_forces = 0  # forces on the foot
-        self.l1 = 15  # thigh length
-        self.l2 = 15  # shin length
-        self.L = 30  # body length
-        self.m = 20  # mass of the whole robot
+        self.l1 = .15  # thigh length
+        self.l2 = .15  # shin length
+        self.body_length = .30  # body length
+        self.body_width = .8  # body width
+        self.m = 8  # mass of the whole robot
         self.control_type = 0  # force or mass feedback?
         self.state = 0  # state machine state
-        self.out1 = 0  # controller output 1
-        self.out2 = 0  # controller output 2
-        self.nr_err = .001  # newton-rhapson error
         self.nr_iterlim = 15  # newton-rhapson iteration limit
         self.bez_res = 100  # bezier resolution
         self.transition_signal = False  # signal from the gait scheduler to transition between states
         self.avg_force = np.zeros(2)  # moving average of the joint forces
         self.contact_thresh = 1  # threshold for contact detection
         self.contact = False  # is the foot in contact with the ground?
+        self.front_or_rear = 1  # is the leg on the front or rear of the robot? 1 for front, -1 for rear
+
         # init state
-        self.init_pos = np.array([-2, 8])  # foot's init state position
+        self.init_pos = np.array([0, .2])  # foot's init state position
+
         # flight state
         self.liftoff_time = 1  # time the foot last left the ground
         self.t_flight = .2  # time the foot has been in the flight state
-        bezier_x = np.array([-0.2, -0.259, -0.275, -0.384, 0.261, -0.017, 0.248, 0.267, 0.259, 0.2])*10
-        bezier_y = np.array([0.5, 0.45, 0.406, 0.065, 1.031, -0.095, 0.545, 0.374, 0.45, 0.5])*10 + self.init_pos[1]
-        self.bezier_pts = np.array([bezier_x, bezier_y])
-        [self.flight_traj_x,
-         self.flight_traj_y] = self.bezier(self.bezier_pts[0, :],
-                                           self.bezier_pts[1, :])
+        self.bezier_x = np.array([
+            -0.2, -0.259, -0.275, -0.384, 0.261, -0.017, 0.248, 0.267, 0.259,
+            0.2
+        ])  # MIT values
+        self.bezier_y = np.array(
+            [0.5, 0.45, 0.406, 0.065, 1.031, -0.095, 0.545, 0.374, 0.45,
+             0.5])  # MIT values
+        self.bezier_x = self.bezier_x * ((self.l1 + self.l2) / .5)  # scale points to fit our robot's leg length
+        self.bezier_y = self.bezier_y * ((self.init_pos[1]) / .5)  # scale points to fit our robot's ride height
+        self.bezier_y = self.bezier_y   # change the coordinate system from MIT's to mine
+        self.bezier_pts = np.array([self.bezier_x, self.bezier_y])
+        self.base_bezier = self.bezier(self.bezier_pts[0, :],
+                                       self.bezier_pts[1, :])
+        self.flight_traj_x = self.base_bezier[0]
+        self.flight_traj_y = self.base_bezier[1]
+        self.flight_traj_idx = np.zeros_like(self.flight_traj_x)
+
         # stance state
         self.touchdown_time = 0  # time the foot last touched the ground
-        self.Tst = .1  # length of the last stance phase
+        self.Tst = .15  # length of the last stance phase
         self.Tsw = .22  # pulled from cheetah paper
         self.Tair = .05  # time both feet were in the air
         self.T = self.Tst + self.Tsw + self.Tair  # time of a total cycle
-        self.ax = .1  # x force amplitude - patrick's thesis says this is user selected, not sure why?
-        self.yhip_des = 15  # desired robot hip ride height
+        self.ax = 0  # x force amplitude - patrick's thesis says this is user selected, not sure why?
+        self.yhip_des = 20  # desired robot hip ride height
         self.thetad_des = 0  # desired pitch oscillation speed
         self.k_stab = 1  # gait pattern stabilizer gain
-        self.kpy_hip = 1  # ride height p gain
-        self.kdy_hip = 1  # ride height d gain
-        self.kdx_v = 1  # velocity p gain
-        self.kp_th = 1  # pitch oscillation p gain
-        self.kd_th = 1  # pitch oscillation d gain
-        self.curr_force = np.zeros(2)  # current environmental forces on the joints
+        self.kpy_hip = 0  #.5  # ride height p gain
+        self.kdy_hip = 0  #.5  # ride height d gain
+        self.kdx_v = 0  #.5  # velocity p gain
+        self.kp_th = 0  #.5  # pitch oscillation p gain
+        self.kd_th = 0  #.5  # pitch oscillation d gain
+        self.curr_force = np.zeros(
+            2)  # current environmental forces on the joints
 
     def run_controller(self, t):
-        # init state
+        # handle transitions
+        # init --> stance
         if self.state == 0:
-            # transition out of the stance state
-            if self.transition_signal:
-                self.transition_signal = False
-                self.transitions(1,t)
-            return self.init(t)
-        # flight state
+            if self.transition_signal:  #t > .5:#self.transition_signal:
+                self.transitions(0, t)
+        # flight --> stance
         elif self.state == 1:
-            if t >= self.liftoff_time + self.t_flight:
-                self.transitions(1, t)
-            return self.flight(t)
-        # stance state
-        elif self.state == 2:
             if self.transition_signal:
-                self.transition_signal = False
-                self.transitions(2,t)
+                self.transitions(1, t)
+        # stance -- > flight
+        elif self.state == 2:
+            if self.transition_signal:  #not self.contact:  #self.transition_signal:
+                self.transitions(2, t)
+
+        # run the appropriate controller
+        if self.state == 0:
+            return self.init(t)
+        elif self.state == 1:
+            return self.flight(t)
+        elif self.state == 2:
             return self.stance(t)
 
     def transitions(self, transition, t):
+        self.transition_signal = False
         # transition init --> flight
         if transition == 0:
-            # switch the state to flight
-            self.state = 1
-            # generate the bezier curve
-            #[self.flight_traj_x,self.flight_traj_y] = self.bezier(self.bezier_pts[0,:],self.bezier_pts[1,:])
-            # update liftoff time
-            self.liftoff_time = t
+            # switch the state to stance
+            self.state = 2
+            # update the touchdown time
+            self.touchdown_time = t
+            # switch to force feedback
+            self.control_type = 1
+            # apply gait pattern stabilization
+            self.Tst = self.body_length / self.v_des - self.k_stab * (
+                self.Tst + self.Tair - self.T / 2)
         # transition flight --> stance
         elif transition == 1:
             # switch the state to stance
@@ -100,14 +122,54 @@ class leg_controller:
             self.touchdown_time = t
             # switch to force feedback
             self.control_type = 1
+            # apply gait pattern stabilization
+            self.Tst = self.body_length / self.v_des - self.k_stab * (
+                self.Tst + self.Tair - self.T / 2)
         # transition stance --> flight
         elif transition == 2:
+            # ----- Flags and value updates -----
             # switch to position feedback
             self.control_type = 0
             # drop the contact flag
             self.contact = False
             # switch the state to flight
-            self.state = 0
+            self.state = 1
+            # update the liftoff time
+            self.liftoff_time = t
+
+            # ----- Trajectory Planning -----
+            foot_pos = self.joint_to_cartesian(self.curr_pos[0],
+                                               self.curr_pos[1])
+            foot_vel = self.joint_to_cartesian(self.curr_vel[0],
+                                               self.curr_vel[1])
+            # generate the base bezier curve
+            bezier_base = self.bezier(self.bezier_x, self.bezier_y)
+            # generate the correction polynomial (really just a line, but MIT calls it a polynomial for some reason)
+            correction_x = np.linspace(foot_pos[0] - bezier_base[0, 0], 0)
+            correction_y = np.linspace(foot_pos[1] - bezier_base[1, 1], 0)
+            correction_x = np.pad(correction_x,
+                                  (0, self.bez_res - np.size(correction_x)),
+                                  'constant',
+                                  constant_values=(0, 0))
+            correction_y = np.pad(correction_y,
+                                  (0, self.bez_res - np.size(correction_y)),
+                                  'constant',
+                                  constant_values=(0, 0))
+            bezier_correction = np.array([correction_x, correction_y])
+            # combine the generated trajectories
+            self.flight_traj_x = bezier_base[0] + bezier_correction[0]
+            self.flight_traj_y = bezier_base[1] + bezier_correction[1]
+            # phase-warp the trajectory (scale s nonlinearly to match touchdown velocity)
+            bezier_x_dot = np.gradient(
+                bezier_base[0]
+            ) * self.bez_res / self.t_flight  # second part is unit conversion to seconds!!
+            self.flight_traj_idx = self.bezier(
+                np.array([
+                    0, 1 / 3 * bezier_x_dot[0]**(-1) * foot_vel[0],
+                    1 - 1 / 3 * bezier_x_dot[-1]**(-1) * self.v_des, 1
+                ]), np.array([0, 0, 0, 0]))
+            self.flight_traj_idx = self.flight_traj_idx[0]
+            print(f"Transition Position: {foot_pos} Transition length: {np.linalg.norm(foot_pos)}")
 
     # init state for the leg - just stands there
     def init(self, t):
@@ -120,12 +182,20 @@ class leg_controller:
     # flight state for the leg - position control along a bezier curve
     def flight(self, t):
         # update the position along the bezier trajectory
+        # calculate the proper index to fetch from the phase-warped bezier s
         flight_percent = (t - self.liftoff_time) / self.t_flight
-        flight_idx = round(self.bez_res * flight_percent)
-        if flight_idx > (np.size(self.flight_traj_x) - 1):
-            flight_idx = np.size(self.flight_traj_x) - 1
-        x = self.flight_traj_x[flight_idx]
-        y = self.flight_traj_y[flight_idx]
+        flight_percent_idx = round(self.bez_res * flight_percent)
+        # make sure the index is within the bounds of the trajectory length
+        if flight_percent_idx > (np.size(self.flight_traj_x) - 1):
+            flight_percent_idx = np.size(self.flight_traj_x) - 1
+            # get the phase-warped s value and make sure it's within the bounds of the trajectory length
+        flight_s = round(self.flight_traj_idx[flight_percent_idx] *
+                         self.bez_res)
+        if flight_s > (np.size(self.flight_traj_x) - 1):
+            flight_s = np.size(self.flight_traj_x) - 1
+            # get the x and y positions from the bezier trajectory
+        x = self.flight_traj_x[flight_s]
+        y = self.flight_traj_y[flight_s]
 
         # convert bezier (x,y) to joints (th1,th2)
         return self.cartesian_to_joint(self.curr_pos, x, y)
@@ -133,19 +203,19 @@ class leg_controller:
     # stance state for the leg - impulse control
     def stance(self, t):
         #unpack/calculate necessary state info
+        foot_pos = self.joint_to_cartesian(self.curr_pos[0], self.curr_pos[1])
         theta = self.pitch
         thetad = self.pitch_vel
         xd = self.forw_vel
         yhip = 5
         ydhip = 5
+        tbar = t - self.touchdown_time
 
-        #calculate force profile amplitudes
+        #calculate force profile amplitude
         ay = .25 * self.m * 9.81 * np.pi * self.T / self.Tst
         #calculate stance feedforward values
-        fx_ff = ay * np.sin(np.pi * t / self.Tst)
-        fy_ff = self.ax * np.sin(np.pi * t / self.Tst)
-        fx_ff = 1
-        fy_ff = 1
+        fx_ff = self.ax * np.sin(np.pi * tbar / self.Tst)
+        fy_ff = ay * np.sin(np.pi * tbar / self.Tst)
 
         #calculate the hip height stabilizing feedback force
         tbar = t - self.touchdown_time
@@ -158,21 +228,15 @@ class leg_controller:
         fx_v = -self.kdx_v * (xd - self.v_des)
         fy_v = 0
         #calculate the pitch stabilizing feedback force
-        #fx_th = 0
-        #fy_th = 1/(x-x_foot) * (self.kp_th*theta + self.kd_th*(thetad - self.thetad_des))
-
-        #gait pattern stabilizer
-        self.Tst = self.L / self.v_des - self.k_stab * (self.Tst + self.Tair -
-                                                        self.T / 2)
+        fx_th = 0
+        fy_th = 0  #1/(-foot_pos[0]*self.front_or_rear) * (self.kp_th*theta + self.kd_th*(thetad - self.thetad_des))
 
         #Calculate the final output of the controller
-        fx_des = fx_ff  # + fx_hip + fx_v+ fx_th
-        fy_des = fy_ff  # + fy_hip + fy_v+ fy_th
+        fx_des = (fx_ff + fx_hip + fx_v + fx_th) / 2
+        fy_des = (fy_ff + fy_hip + fy_v + fy_th) / 2
+        #print(f"x: {fx_des}     y: {fy_des}")
 
-        fx_des = 0
-        fy_des = 1
-        joint_forces = self.force_map(np.array([fx_des, fy_des]))
-        joint_forces = np.ones(2)
+        joint_forces = self.force_map(np.flip(np.array([fx_des, fy_des])))
 
         return joint_forces
 
@@ -184,10 +248,10 @@ class leg_controller:
     def jacobian(self, th_vector):
         th1 = th_vector[0]
         th2 = th_vector[1]
-        j_11 = -self.l2 * np.sin(th1) - self.l2 * np.sin(th1 + th2)
-        j_12 = -self.l1 * np.sin(th1 + th2)
-        j_21 = self.l2 * np.cos(th1) + self.l2 * np.cos(th1 + th2)
-        j_22 = self.l1 * np.cos(th1 + th2)
+        j_11 = self.l1 * np.cos(th1) + self.l2 * np.cos(th1 + th2)
+        j_12 = self.l2 * np.cos(th1 + th2)
+        j_21 = -self.l1 * np.sin(th1) - self.l2 * np.sin(th1 + th2)
+        j_22 = -self.l2 * np.sin(th1 + th2)
         J = np.array([[j_11, j_12], [j_21, j_22]])
         return J
 
@@ -207,27 +271,23 @@ class leg_controller:
             th2 = 0
 
         # calculate the forward kinematics
-        f_11 = self.l1 * np.cos(th1) + self.l2 * np.cos(th1 + th2) - self.x_inv
-        f_21 = self.l1 * np.sin(th1) + self.l2 * np.sin(th1 + th2) - self.y_inv
-        f = np.array([f_11, f_21])
+        f = self.joint_to_cartesian(th1, th2) - np.array(
+            [self.x_inv, self.y_inv])
 
         return f
 
     # Inverse Kinematics - Solution
     def cartesian_to_joint(self, th_guess, x_inv, y_inv):
         # unpack the cartesian points and account for the origin/orientation transform
-        self.x_inv = -x_inv
-        self.y_inv = -y_inv + self.l1 + self.l2
+        self.x_inv = x_inv  #-x_inv
+        self.y_inv = y_inv  #-y_inv + self.l1 + self.l2
 
         # check if the desired output is outside of the joint space
-        if np.sqrt(self.x_inv**2 +
-                   (self.y_inv - self.l1 - self.l2)**2) > (self.l1 + self.l2):
-            th1 = np.pi / 4
-            th2 = -np.pi / 2
-            print(
-                "Inverse kinematics are asking the leg to go somewhere it can't reach!"
-            )
-            return np.array([th1, th2])
+        if np.linalg.norm([x_inv,y_inv]) > (self.l1 + self.l2):
+            #print(
+            #    f"Inverse kinematics are asking the leg to go somewhere it can't reach! Inputs: {x_inv}, {y_inv}"
+            #)
+            return self.curr_pos
 
         # solve the inverse kinematics problem
         solution = fsolve(func=self.f,
@@ -235,30 +295,27 @@ class leg_controller:
                           fprime=self.jacobian,
                           full_output=False,
                           xtol=1e-5)
-        solution[0] = solution[0] % np.pi - np.pi / 2
-        solution[1] = solution[1] % np.pi
+        solution[0] = solution[0]
+        solution[1] = solution[1]
         return solution
 
     # Forward kinematics - joint space to cartesian space
     def joint_to_cartesian(self, th1, th2):
 
         # calculate x and y positions
-        y = self.l2 * np.cos(th1 + th2) + self.l1 * np.cos(th1)
         x = self.l2 * np.sin(th1 + th2) + self.l1 * np.sin(th1)
-
-        # shift the origin down to the leg's zero point
-        y = self.l1 + self.l2 - y
+        y = self.l2 * np.cos(th1 + th2) + self.l1 * np.cos(th1)
 
         return np.array([x, y])
 
     # grf to joint torques - grf space to joint space
     def force_map(self, F):
-        transformed_curr_pos = self.curr_pos - np.array([np.pi / 2,0])
+        transformed_curr_pos = self.curr_pos - np.array([np.pi / 2, 0])
         # calculate the Jacobian
         j = self.jacobian(transformed_curr_pos)
 
         # Transform from grf space to joint space
-        torque = np.dot(np.linalg.inv(np.transpose(j)),F)
+        torque = -np.dot(np.transpose(j), F)
 
         return torque
 
@@ -300,5 +357,27 @@ class leg_controller:
             (abs(self.curr_force[0]) +
              abs(self.curr_force[1]))) > self.contact_thresh:
             self.contact = True
-        else:
+        self.contact = False
+
+    # Liftoff detection - checks if the hip is decelerating to decide if it's in the air
+    def detect_liftoff(self):
+        # calculate the hip and the foot's vertical velocities
+        foot_vel = self.joint_to_cartesian(self.curr_vel[0], self.curr_vel[1])
+        if self.name == "Front Right":
+            yd_hip = self.body_length * np.sin(
+                self.pitch_vel) + self.body_width * np.sin(self.roll_vel)
+        elif self.name == "Front Left":
+            yd_hip = self.body_length * np.sin(
+                self.pitch_vel) - self.body_width * np.sin(self.roll_vel)
+        elif self.name == "Rear Right":
+            yd_hip = -self.body_length * np.sin(
+                self.pitch_vel) + self.body_width * np.sin(self.roll_vel)
+        elif self.name == "Rear Left":
+            yd_hip = -self.body_length * np.sin(
+                self.pitch_vel) - self.body_width * np.sin(self.roll_vel)
+        
+        # if the hip is falling and the foot is pushing down, we've lost contact
+        if (yd_hip < 0) and (foot_vel[1] < 0):
             self.contact = False
+        else:
+            pass
--- pd_controller.py
+++ pd_controller.py
@@ -3,30 +3,23 @@ import numpy as np
 
 
 class PD:
-
+    
     def __init__(self):
-        self.kp_pos = 25
-        self.kd_pos = 1
-        self.kp_force = 25
-        self.kd_force = 1
+        self.kp_pos = 200
+        self.kd_pos = 4
         self.pos_ma = 0
         self.pos_ma_window = 1
-        self.force_ma = 0
-        self.force_ma_window = 1
 
     def run_pd(self, leg, des, act, vel):
         # choose the proper controller based on the feedback variable
         if leg.control_type == 0:
             return self.pd_pos(des, act, vel)
         elif leg.control_type == 1:
-            return self.pd_force(des, act, vel)
+            return self.pd_force(des)
 
-    # pd control for force
-    def pd_force(self, force_des, force_curr, vel_curr):
-        force_ma = self.force_ma*(self.force_ma_window - 1) + force_curr/self.force_ma_window
-        return np.multiply(self.kp_force,
-                           (force_des - force_curr)) - np.multiply(
-                               self.kd_force, vel_curr)
+    # the mujoco actuators used are motors, so we need to do no pd control here
+    def pd_force(self, force_des):
+        return force_des
 
     # pd control for position
     def pd_pos(self, pos_des, pos_curr, vel_curr):
